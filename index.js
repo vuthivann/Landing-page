7b9ffcd7662cbf956a5c39f29f0357692ebe49f0
vuthivann <yong.vuthivann19@kit.edu.kh>
2020-12-23 16:48:38 +0700

    ignore

--- index.js
+++ index.js
@@ -45,3 +45,75 @@ const storeScroll = () => {
 document.addEventListener("scroll", debounce(storeScroll), { passive: true });
 
 storeScroll();
+
+
+
+// slider
+const slides = document.querySelectorAll(".c_slide");
+
+const preBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector ("#nextBtn");
+const indicatorParent = document.querySelector('.c_dot '); 
+const indicators = document.querySelectorAll('.c_dot li');
+let index = 0;
+
+slides.forEach(function(slide, index){
+    slide.style.left= `${index*100}%`;
+});
+
+// function for swipe 
+
+function carousel(){
+    if (index === slides.length){
+        index = 0;
+    }
+    if (index < 0 ){
+        index = slides.length -1 ;
+    }
+
+    slides.forEach(function(slide){
+        slide.style.transform= `translateX(-${index*100}%)`;
+    });
+
+    document.querySelector('.c_dot .c_selected').classList.remove('c_selected');
+    indicatorParent.children[index].classList.add('c_selected');
+    // setIndex(index);
+   
+}
+
+// auto slide function 
+
+function autoslide(){
+    index++;
+    carousel();
+    setTimeout(autoslide, 5000);
+
+};
+
+autoslide();
+
+// dot point function 
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+    slides.forEach(function(slide){
+        slide.style.transform= `translateX(-${index*100}%)`;
+    });
+    
+    document.querySelector('.c_dot .selected').classList.remove('selected');
+    indicatorParent.children[index].classList.add('selected');
+    //   index = i;
+    });
+});
+
+// btn add event listener for swipe card 
+nextBtn.addEventListener("click" ,function(){
+    index++;
+    carousel();
+
+
+});
+preBtn.addEventListener("click" ,function(){
+    index--;
+    carousel();
+
+});
